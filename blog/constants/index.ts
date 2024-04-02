133109c80c8375625ec693211a2e154e0f1b7c64
kumanboy <husandavronov13@gmail.com>
2024-04-02 10:56:01 +0500

    all pages end

--- blog/constants/index.ts
+++ blog/constants/index.ts
@@ -1,31 +1,25 @@
-import {
-	Contact2,
-	FileCode2,
-	FolderArchive,
-	Home,
-	ListCollapse,
-} from 'lucide-react'
+import {Contact2, FileCode2, FolderArchive, Home, ListCollapse,} from 'lucide-react'
 
 export const navLinks = [
-	{ name: 'Home', route: '/', icon: Home },
-	{ name: 'About', route: '/about', icon: ListCollapse },
-	{ name: 'Blogs', route: '/blogs', icon: FileCode2 },
-	{ name: 'Archive', route: '/blogs/archive', icon: FolderArchive },
-	{ name: 'Contact', route: '/contact', icon: Contact2 },
+	{name: 'Home', route: '/', icon: Home},
+	{name: 'About', route: '/about', icon: ListCollapse},
+	{name: 'Blogs', route: '/blogs', icon: FileCode2},
+	{name: 'Archive', route: '/blogs/archive', icon: FolderArchive},
+	{name: 'Contact', route: '/contact', icon: Contact2},
 ]
 
 export const popularCategories = [
-	{ name: 'Front End', slug: 'front-end' },
-	{ name: 'Back End', slug: 'back-end' },
-	{ name: 'Full Stack', slug: 'full-stack' },
-	{ name: "Sun'iy Intelekt", slug: 'artificial-intelligence' },
+	{name: 'Front End', slug: 'front-end'},
+	{name: 'Back End', slug: 'back-end'},
+	{name: 'Full Stack', slug: 'full-stack'},
+	{name: "Sun'iy Intelekt", slug: 'artificial-intelligence'},
 ]
 
 export const popularTags = [
-	{ name: 'ReactJS', slug: 'react-js' },
-	{ name: 'JavaScript', slug: 'java-script' },
-	{ name: 'NodeJS', slug: 'node-js' },
-	{ name: 'NextJS', slug: 'next-js' },
+	{name: 'ReactJS', slug: 'react-js'},
+	{name: 'JavaScript', slug: 'java-script'},
+	{name: 'NodeJS', slug: 'node-js'},
+	{name: 'NextJS', slug: 'next-js'},
 ]
 
 export const blogs = [
@@ -82,4 +76,6 @@ export const authors = [
 		image: '/author/itadori.jpg',
 	},
 ]
+export const content =
+	'<p></p><table><tbody><tr><td><h5><em><strong>Segment</strong></em></h5></td><td><h5><strong>Long-fruited</strong></h5></td></tr><tr><td><h5><em><strong>Purpose</strong></em></h5></td><td><h5><strong>For fresh consumption and processing</strong></h5></td></tr><tr><td><h5><em><strong>Precocity</strong></em></h5></td><td><h5><strong>Early ripening, 60-63 days after planting seedlings</strong></h5></td></tr><tr><td><h5><em><strong>Plant</strong></em></h5></td><td><h5><strong>Vigorous, open</strong></h5></td></tr><tr><td><h5><em><strong>Fruit</strong></em></h5></td><td><h5><strong>Elongated teardrop-shaped, with dense pulp</strong></h5></td></tr><tr><td><h5><em><strong>Color</strong></em></h5></td><td><h5><strong>Dark purple to black</strong></h5></td></tr><tr><td><h5><em><strong>Weight</strong></em></h5></td><td><h5><strong>200-250 g</strong></h5></td></tr><tr><td><h5><em><strong>Taste</strong></em></h5></td><td><h5><strong>Good</strong></h5></td></tr></tbody></table><p></p><blockquote><strong>ADVANTAGES</strong></blockquote><ul><li><div><strong>Early ripeness</strong></div></li><li><div><strong>Very high yield potential</strong></div></li><li><div><strong>Attractive fruits</strong></div></li><li><div><strong>Great for slicing</strong></div></li></ul><blockquote><strong>RECOMMENDATIONS FOR GROWING</strong></blockquote><ul><li><div><strong>Designed for cultivation in open ground,</strong></div><div><strong>as well as in plastic greenhouses</strong></div></li></ul><p></p>'
 
