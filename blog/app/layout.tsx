15057a6c376d3c49a802205f243390c6d9a167b0
Husan Davronov <87082029+kumanboy@users.noreply.github.com>
2024-05-03 15:11:27 +0500

    SEO updated

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
         type: 'website',
         url: 'https://talimot.uz',
         locale: 'en_EN',
-        images: 'https://us-east-1-shared-usea1-02.graphassets.com/cluhz35hg2tcy08licbj9djj2/clvqi903n09b007jzo8jygwcf',
+        images: 'https://us-east-1-shared-usea1-02.graphassets.com/cluhz35hg2tcy08licbj9djj2/clvng2vxy14u907lh5hr5wn41',
         countryName: 'Uzbekistan',
         siteName: 'Next Blog',
         emails: 'info@next.edu',
